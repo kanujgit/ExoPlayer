5ee5828e4bd6f2946bb77ba614b97cd5d201d712
Anuj <anujkumar30dec@gmail.com>
2019-01-27 15:50:03 +0530

    update progress loader when download

--- app/src/main/java/com/anuj/exoplayer/DownloadAndEncryptFileTask.java
+++ app/src/main/java/com/anuj/exoplayer/DownloadAndEncryptFileTask.java
@@ -1,5 +1,7 @@
 package com.anuj.exoplayer;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -19,17 +21,36 @@ import javax.crypto.CipherOutputStream;
 
 public class DownloadAndEncryptFileTask extends AsyncTask<Void, Void, Void> {
 
+    Context context;
     private String mUrl;
     private File mFile;
     private Cipher mCipher;
+    ProgressDialog mProgressDialog;
 
-    public DownloadAndEncryptFileTask(String url, File file, Cipher cipher) {
+    public DownloadAndEncryptFileTask(Context context,String url, File file, Cipher cipher) {
         if (url == null || url.isEmpty()) {
             throw new IllegalArgumentException("You need to supply a url to a clear MP4 file to download and encrypt, or modify the code to use a local encrypted mp4");
         }
+        context =context;
         mUrl = url;
         mFile = file;
         mCipher = cipher;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        // Create progress dialog
+        mProgressDialog = new ProgressDialog(context);
+        // Set your progress dialog Title
+        mProgressDialog.setTitle("Downloading");
+        // Set your progress dialog Message
+        mProgressDialog.setMessage("Downloading, Please Wait!");
+        mProgressDialog.setIndeterminate(false);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        // Show progress dialog
+        mProgressDialog.show();
     }
 
     private void downloadAndEncrypt() throws Exception {
@@ -37,6 +58,9 @@ public class DownloadAndEncryptFileTask extends AsyncTask<Void, Void, Void> {
         URL url = new URL(mUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.connect();
+
+        int fileLength = connection.getContentLength();
+
 
         if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
             throw new IOException("server error: " + connection.getResponseCode() + ", " + connection.getResponseMessage());
@@ -46,12 +70,31 @@ public class DownloadAndEncryptFileTask extends AsyncTask<Void, Void, Void> {
         FileOutputStream fileOutputStream = new FileOutputStream(mFile);
         CipherOutputStream cipherOutputStream = new CipherOutputStream(fileOutputStream, mCipher);
 
+        long total =0;
         byte buffer[] = new byte[1024 * 1024];
         int bytesRead;
         while ((bytesRead = inputStream.read(buffer)) != -1) {
+
             Log.d(getClass().getCanonicalName(), "reading from http...");
+
+            total += bytesRead;
+
+
             cipherOutputStream.write(buffer, 0, bytesRead);
         }
+
+
+//
+//        long total = 0;
+//        int count;
+//        while ((count = inputStream.read(buffer)) != -1) {
+//            total += count;
+//            // Publish the progress
+//            //publishProgress((int) (total * 100 / fileLength));
+//            //output.write(data, 0, count);
+//        }
+
+
 
         inputStream.close();
         cipherOutputStream.close();
@@ -70,6 +113,8 @@ public class DownloadAndEncryptFileTask extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected void onPostExecute(Void aVoid) {
+
+        mProgressDialog.dismiss();
         Log.d(getClass().getCanonicalName(), "done");
     }
 }
--- app/src/main/java/com/anuj/exoplayer/VideoActivity.java
+++ app/src/main/java/com/anuj/exoplayer/VideoActivity.java
@@ -1,6 +1,10 @@
 package com.anuj.exoplayer;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,9 +13,15 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -28,7 +38,6 @@ public class VideoActivity extends AppCompatActivity {
     private Cipher mCipher;
     private SecretKeySpec mSecretKeySpec;
     private IvParameterSpec mIvParameterSpec;
-
 
 
     @Override
@@ -76,14 +85,14 @@ public class VideoActivity extends AppCompatActivity {
 
     private void createVideoList() {
 
-        taskModels.add(new TaskModel(0,"first" ,"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"));
-        taskModels.add(new TaskModel(1, "Second","http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"));
-        taskModels.add(new TaskModel(2, "third","http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"));
+        taskModels.add(new TaskModel(0, "first", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"));
+        taskModels.add(new TaskModel(1, "Second", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"));
+        taskModels.add(new TaskModel(2, "third", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"));
 
     }
 
     public void encryptVideo(int position, File file) {
-        Log.e("file_name",file.getName()+" //"+file.getAbsolutePath());
+        Log.e("file_name", file.getName() + " //" + file.getAbsolutePath());
         if (hasFile(file)) {
             Log.d(getClass().getCanonicalName(), "encrypted file found, no need to recreate");
             Toast.makeText(this, "encrypted file found, no need to recreate", Toast.LENGTH_SHORT).show();
@@ -98,7 +107,8 @@ public class VideoActivity extends AppCompatActivity {
                 // the ciphers, key and iv used in this demo, or to see it from top to bottom,
                 // supply a url to a remote unencrypted file - this method will download and encrypt it
                 // this first argument needs to be that url, not null or empty...
-                new DownloadAndEncryptFileTask(taskModels.get(position).getUrl(), file, encryptionCipher).execute();
+                // new DownloadAndEncryptFileTask(VideoActivity.this, taskModels.get(position).getUrl(), file, encryptionCipher).execute();
+                new DownloadFilesTask(file, encryptionCipher).execute(taskModels.get(position).getUrl());
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -110,5 +120,123 @@ public class VideoActivity extends AppCompatActivity {
         return mEncryptedFile != null
                 && mEncryptedFile.exists()
                 && mEncryptedFile.length() > 0;
+    }
+
+
+    private class DownloadFilesTask extends AsyncTask<String, Integer, Long> {
+
+        ProgressDialog mProgressDialog;
+        private PowerManager.WakeLock mWakeLock;
+
+
+        private File mFile;
+        private Cipher mCipher;
+
+        public DownloadFilesTask(File mFile, Cipher mCipher) {
+            this.mFile = mFile;
+            this.mCipher = mCipher;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            super.onPreExecute();
+            // Create progress dialog
+            mProgressDialog = new ProgressDialog(VideoActivity.this);
+            // Set your progress dialog Title
+            mProgressDialog.setTitle("Downloading");
+            // Set your progress dialog Message
+            mProgressDialog.setMessage("Downloading, Please Wait!");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            // Show progress dialog
+            mProgressDialog.setCancelable(false);
+
+
+            // take CPU lock to prevent CPU from going off if the user
+            // presses the power button during download
+            PowerManager pm = (PowerManager) (VideoActivity.this).getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    getClass().getName());
+            //mWakeLock.acquire(10*60*1000L /*10 minutes*/);
+            mWakeLock.acquire();
+            mProgressDialog.show();
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            // if we get here, length is known, now set indeterminate to false
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgress(values[0]);
+        }
+
+        protected Long doInBackground(String... Url) {
+
+            long total = 0;
+            try {
+                URL url = new URL(Url[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                // Detect the file lenghth
+                int fileLength = connection.getContentLength();
+
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    throw new IOException("server error: " + connection.getResponseCode() + ", " + connection.getResponseMessage());
+                }
+
+                InputStream inputStream = connection.getInputStream();
+                FileOutputStream fileOutputStream = new FileOutputStream(mFile);
+                CipherOutputStream cipherOutputStream = new CipherOutputStream(fileOutputStream, mCipher);
+
+
+                byte buffer[] = new byte[1024 * 1024];
+
+
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+
+                    if (isCancelled()) {
+                        inputStream.close();
+                        return null;
+                    }
+                    Log.d(getClass().getCanonicalName(), "reading from http...");
+
+                    total += bytesRead;
+
+                    Log.e("toal", total + "///" + fileLength);
+                    if (fileLength > 0)
+                        publishProgress((int) (total * 100 / fileLength));
+                    cipherOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                inputStream.close();
+                cipherOutputStream.close();
+                connection.disconnect();
+
+
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+
+
+            return total;
+        }
+
+        protected void onPostExecute(Long result) {
+            //   showDialog(("Downloaded " + result + " bytes"));
+            mProgressDialog.dismiss();
+            mWakeLock.release();
+
+            if (result != null)
+                Toast.makeText(VideoActivity.this, "File downloaded", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(VideoActivity.this, "Download error: " + result, Toast.LENGTH_LONG).show();
+
+        }
     }
 }
